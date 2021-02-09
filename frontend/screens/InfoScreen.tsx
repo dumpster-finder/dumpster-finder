8b132ff6b10a6daa591fff72b96a6de4c855d5e4
heleneyj <heleneyj@stud.ntnu.no>
2021-02-09 17:56:33 +0100

    Add settings page

--- frontend/screens/InfoScreen.tsx
+++ frontend/screens/InfoScreen.tsx
@@ -1,140 +1,156 @@
-import * as React from 'react';
-import {Image, StyleSheet, Switch} from 'react-native';
+import * as React from "react";
+import {Image, StyleSheet, Switch} from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import {  View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import {View} from "../components/Themed";
 import {Card, Icon, Text} from "react-native-elements";
 
 export default function InfoScreen() {
-  return (
-    <View style={styles.container}>
-        <View>
-          <Text h1 style={{alignSelf: 'center'}}>Info</Text>
-          <View style={{
-            height: '100%',
-            width: 400,
-            flex: 1,
-            alignItems:'center',
-            flexDirection: 'column',
-          }}>
-            <View style={{
-              width: '100%',
-              flex: 1,
-              flexDirection: 'row',
-              alignItems:'center'
-            }}>
-              <View style={{width: '20%', height:'100%'}} >
-                <Icon name='star' color='#FFD100' />
-              </View>
-              <View style={{width: '80%', height:'100%'}} >
-                <Text>Rating for dumpster</Text>
-              </View>
-            </View>
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text h1 style={{alignSelf: "center"}}>
+                    Info
+                </Text>
+                <View
+                    style={{
+                        height: "100%",
+                        width: 400,
+                        flex: 1,
+                        alignItems: "center",
+                        flexDirection: "column",
+                    }}>
+                    <View
+                        style={{
+                            width: "100%",
+                            flex: 1,
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}>
+                        <View style={{width: "20%", height: "100%"}}>
+                            <Icon name="star" color="#FFD100" />
+                        </View>
+                        <View style={{width: "80%", height: "100%"}}>
+                            <Text>Rating for dumpster</Text>
+                        </View>
+                    </View>
 
-            <View style={{
-              width: '100%',
-              flex: 1,
-              flexDirection: 'row',
-              alignItems:'center',
-            }}>
-              <View style={{width: '20%', height:'100%'}} >
-                <Icon name='lock'
-                      color='#FF0000' />
-              </View>
-              <View style={{width: '80%', height:'100%'}} >
-                <Text>The dumpster is locked</Text>
-              </View>
-            </View>
+                    <View
+                        style={{
+                            width: "100%",
+                            flex: 1,
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}>
+                        <View style={{width: "20%", height: "100%"}}>
+                            <Icon
+                                name="lock"
+                                type="font-awesome"
+                                color="#FF0000"
+                            />
+                        </View>
+                        <View style={{width: "80%", height: "100%"}}>
+                            <Text>The dumpster is locked</Text>
+                        </View>
+                    </View>
 
-            <View style={{
-              width: '100%',
-              flex: 1,
-              flexDirection: 'row',
-              alignItems:'center',
-            }}>
-              <View style={{width: '20%', height:'100%'}} >
-                <Icon name='lock-open'
-                      color='#54C500'/>
-              </View>
-              <View style={{width: '80%', height:'100%'}} >
-                <Text>The dumpster is open</Text>
-              </View>
-            </View>
+                    <View
+                        style={{
+                            width: "100%",
+                            flex: 1,
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}>
+                        <View style={{width: "20%", height: "100%"}}>
+                            <Icon
+                                name="unlock"
+                                type="font-awesome"
+                                color="#54C500"
+                            />
+                        </View>
+                        <View style={{width: "80%", height: "100%"}}>
+                            <Text>The dumpster is open</Text>
+                        </View>
+                    </View>
 
-            <View style={{
-              width: '100%',
-              flex: 1,
-              flexDirection: 'row',
-              alignItems:'center',
-            }}>
-              <View style={{width: '20%', height:'100%'}} >
-                <Icon name='edit'/>
-              </View>
-              <View style={{width: '80%', height:'100%'}} >
-                <Text>Edit the dumpster</Text>
-              </View>
-            </View>
+                    <View
+                        style={{
+                            width: "100%",
+                            flex: 1,
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}>
+                        <View style={{width: "20%", height: "100%"}}>
+                            <Icon name="edit" type="font-awesome" />
+                        </View>
+                        <View style={{width: "80%", height: "100%"}}>
+                            <Text>Edit the dumpster</Text>
+                        </View>
+                    </View>
 
-            <View style={{
-              width: '100%',
-              flex: 1,
-              flexDirection: 'row',
-              alignItems:'center',
-            }}>
-              <View style={{width: '20%', height:'100%'}} >
-                <Icon name='delete'/>
-              </View>
-              <View style={{width: '80%', height:'100%'}} >
-                <Text>When the dumpster is locked </Text>
-              </View>
-            </View>
+                    <View
+                        style={{
+                            width: "100%",
+                            flex: 1,
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}>
+                        <View style={{width: "20%", height: "100%"}}>
+                            <Icon name="delete" />
+                        </View>
+                        <View style={{width: "80%", height: "100%"}}>
+                            <Text>When the dumpster is emptied </Text>
+                        </View>
+                    </View>
 
-            <View style={{
-              width: '100%',
-              flex: 1,
-              flexDirection: 'row',
-              alignItems:'center',
-            }}>
-              <View style={{width: '20%', height:'100%'}} >
-                <Icon name="thumbs-up" type="font-awesome"/>
-              </View>
-              <View style={{width: '80%', height:'100%'}} >
-                <Text>The shop’s view on dumpster diving </Text>
-              </View>
-            </View>
+                    <View
+                        style={{
+                            width: "100%",
+                            flex: 1,
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}>
+                        <View style={{width: "20%", height: "100%"}}>
+                            <Icon name="thumbs-up" type="font-awesome" />
+                        </View>
+                        <View style={{width: "80%", height: "100%"}}>
+                            <Text>The shop’s view on dumpster diving </Text>
+                        </View>
+                    </View>
 
-            <View style={{
-              height: '50%',
-              width: 400,
-              alignItems: 'center',
-              justifyContent: 'center'
-            }}>
-              <Text h3>Dumpster diving advice:</Text>
-              <Text>1. Don’t go diving in locked dumpsters</Text>
-              <Text>2. Don’t go in dumpsters makred private</Text>
-              <Text>3. Don’t go dumpster diving alone</Text>
-              <Text>4. Dress accordingly</Text>
+                    <View
+                        style={{
+                            height: "50%",
+                            width: 400,
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}>
+                        <Text h3>Dumpster diving advice:</Text>
+                        <Text>1. Don’t go diving in locked dumpsters</Text>
+                        <Text>2. Don’t go in dumpsters makred private</Text>
+                        <Text>3. Don’t go dumpster diving alone</Text>
+                        <Text>4. Dress accordingly</Text>
+                    </View>
+                </View>
             </View>
-          </View>
+            {/*<EditScreenInfo path="/screens/InfoScreen.tsx" />*/}
         </View>
-      {/*<EditScreenInfo path="/screens/InfoScreen.tsx" />*/}
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: "80%",
+    },
 });
--- frontend/screens/ListScreen.tsx
+++ frontend/screens/ListScreen.tsx
@@ -11,10 +11,12 @@ import EditScreenInfo from "../components/EditScreenInfo";
 import {Text, View} from "../components/Themed";
 import ListCards from "../components/ListCards";
 import Dumpster from "../models/Dumpster";
+import {Icon, SearchBar} from "react-native-elements";
+import useColorScheme from "../hooks/useColorScheme";
 
 const a: Dumpster = {
     dumpsterID: 1,
-    name: "My store",
+    name: "Helens' store",
     position: {latitude: 1, longitude: 2},
     emptyingSchedule: "Monday",
     locked: true,
@@ -38,13 +40,46 @@ const b: Dumpster = {
     dumpsterType: "idk",
 };
 
+const c: Dumpster = {
+    dumpsterID: 1,
+    name: "Jons' store",
+    position: {latitude: 1, longitude: 2},
+    emptyingSchedule: "Today",
+    locked: false,
+    positiveStoreViewOnDiving: true,
+    rating: 4.5,
+    cleanliness: 3.5,
+    storeType: "Beds",
+    dumpsterType: "idk",
+};
+
+
+
 export default function ListScreen() {
+    const colorScheme = useColorScheme();
+    const allDumpsters = [a, b, c];
+
     return (
         <ScrollView style={styles.scrollView}>
-            <ListCards dumpster={a}/>
-            <ListCards dumpster={b}/>
-            <ListCards dumpster={a}/>
-            <ListCards dumpster={b}/>
+            <View style={{
+                width: '100%',
+                flex: 1,
+                flexDirection: 'row',
+                alignItems:'center',
+                justifyContent: 'center'
+            }}>
+                <View style={{width: '90%', height:'100%'}} >
+                    <SearchBar
+                        lightTheme={colorScheme === 'light'}
+                        placeholder="Type Here..."
+                        value={''}
+                    />
+                </View>
+                <View style={{width: '10%', height:'100%', justifyContent:'center'}} >
+                    <Icon name='filter' type="font-awesome"/>
+                </View>
+            </View>
+            {allDumpsters.map((thisDumpster) => <ListCards dumpster={thisDumpster}/>)}
             <View
                 style={styles.separator}
                 lightColor="#eee"
