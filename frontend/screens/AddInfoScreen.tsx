034548720d5cc452ceeb56e4f58c0db86b321379
heleneyj <heleneyj@stud.ntnu.no>
2021-02-16 10:15:07 +0100

    Add dynamic info to AddInfoScreen

--- frontend/screens/AddInfoScreen.tsx
+++ frontend/screens/AddInfoScreen.tsx
@@ -1,189 +1,221 @@
 import * as React from "react";
 import {Picker, StyleSheet, Switch, View} from "react-native";
 import {Button, Icon, Input, Slider, Text} from "react-native-elements";
+import {useState} from "react";
 
 export default function AddInfoScreen() {
-return(
-    <View style={styles.container}>
-        <View
-            style={{
-                height: "100%",
-                width: 400,
-                flex: 1,
-                alignItems: "center",
-                flexDirection: "column",
-            }}>
+    const dumpsterTypes = ["Metal", "Compressor", "Plastic"];
+    const storeTypes = ["Food", "Electronics"];
 
-            <View
-                style={{
-                    height: "5%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flex: 1,
-                    flexDirection: "row"
-                }}>
-                <Text>Name:</Text>
-                <Input placeholder="Name" />
-            </View>
+    const [name, onChangeName] = useState("");
+    const [dumpster, onChangeDumpster] = useState(dumpsterTypes[0]);
+    const [store, onChangeStore] = useState(storeTypes[0]);
+    const [empty, onChangeEmpty] = useState("");
+    const [cleanliness, onChangeClean] = useState("50");
+    const [isPositive, setIsPositive] = useState(false);
+    const [isLocked, setIsLocked] = useState(false);
 
+    return (
+        <View style={styles.container}>
             <View
                 style={{
-                    height: "5%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
+                    height: "100%",
+                    width: 400,
                     flex: 1,
-                    flexDirection: "row"
-                }}>
-                <Text>Dumpster type:</Text>
-                <Picker
-                    selectedValue={'Dumpster'}
-                    style={{ height: 50, width: 150 }}
-                    onValueChange={(itemValue, itemIndex) => console.log(itemValue)}
-                    >
-                    <Picker.Item label="Metal" value="metal" />
-                    <Picker.Item label="Compressor" value="compressor" />
-                </Picker>
-            </View>
-            <View
-                style={{
-                    height: "5%",
-                    width: "60%",
                     alignItems: "center",
-                    justifyContent: "center",
-                    flex: 1,
-                    flexDirection: "row"
+                    flexDirection: "column",
                 }}>
-                <Text>Store type:</Text>
-                <Picker
-                    selectedValue={'Dumpster'}
-                    style={{ height: 50, width: 150 }}
-                    onValueChange={(itemValue, itemIndex) => console.log(itemValue)}
-                >
-                    <Picker.Item label="FOOOOOOD" value="food" />
-                    <Picker.Item label="Electronics" value="electro" />
-                </Picker>
-            </View>
-            <View
-                style={{
-                    height: "5%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flex: 1,
-                    flexDirection: "row"
-                }}>
-                <Text>Tags:</Text>
-                <Input placeholder="IDK" />
-            </View>
-            <View
-                style={{
-                    height: "5%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flex: 1,
-                    flexDirection: "row"
-                }}>
-                <Switch value={true} />
-                <Icon name="thumbs-up" type="font-awesome" />
-                <Text>Positive attitude</Text>
-            </View>
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Text>Name:</Text>
+                    <Input
+                        placeholder="Name"
+                        onChangeText={text => onChangeName(text)}
+                        value={name}
+                    />
+                </View>
 
-            <View
-                style={{
-                    height: "5%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flex: 1,
-                    flexDirection: "row"
-                }}>
-                <Switch value={true} />
-                <Icon name="lock" type="font-awesome" />
-                <Text>Locked</Text>
-            </View>
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Text>Dumpster type:</Text>
+                    <Picker
+                        selectedValue={dumpster}
+                        style={{height: 50, width: 150}}
+                        onValueChange={(itemValue: string, itemIndex: number) =>
+                            onChangeDumpster(itemValue)
+                        }>
+                        {dumpsterTypes.map(i => (
+                            <Picker.Item label={i} value={i} />
+                        ))}
+                    </Picker>
+                </View>
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Text>Store type:</Text>
+                    <Picker
+                        selectedValue={store}
+                        style={{height: 50, width: 150}}
+                        onValueChange={(itemValue: string) =>
+                            onChangeStore(itemValue)
+                        }>
+                        {storeTypes.map(i => (
+                            <Picker.Item label={i} value={i} />
+                        ))}
+                    </Picker>
+                </View>
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Text>Tags:</Text>
+                    <Input placeholder="IDK" />
+                </View>
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Switch
+                        onValueChange={() =>
+                            setIsPositive(previousState => !previousState)
+                        }
+                        value={isPositive}
+                    />
+                    <Icon name="thumbs-up" type="font-awesome" />
+                    <Text>Positive attitude</Text>
+                </View>
 
-            <View
-                style={{
-                    height: "5%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flex: 1,
-                    flexDirection: "row"
-                }}>
-                <Icon name="delete" />
-                <Input placeholder="Emptied at times..." />
-            </View>
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Switch
+                        onValueChange={() =>
+                            setIsLocked(previousState => !previousState)
+                        }
+                        value={isLocked}
+                    />
+                    <Icon name="lock" type="font-awesome" />
+                    <Text>Locked</Text>
+                </View>
 
-            <View
-                style={{
-                    height: "5%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flex: 1,
-                    flexDirection: "row"
-                }}>
-                <Text>Cleanliness:</Text>
-                <Slider
-                    animateTransitions
-                    animationType="timing"
-                    maximumTrackTintColor="#ccc"
-                    maximumValue={100}
-                    minimumTrackTintColor="#222"
-                    minimumValue={0}
-                    onSlidingComplete={() =>
-                        console.log("onSlidingComplete()")
-                    }
-                    onSlidingStart={() =>
-                        console.log("onSlidingStart()")
-                    }
-                    onValueChange={value =>
-                        console.log("onValueChange()", value)
-                    }
-                    orientation="horizontal"
-                    step={1}
-                    style={{ width: "60%", height: 200, marginLeft: 5 }}
-                    thumbStyle={{ height: 20, width: 20 }}
-                    thumbTintColor="#0c0"
-                    thumbTouchSize={{ width: 40, height: 40 }}
-                    trackStyle={{ height: 10, borderRadius: 20 }}
-                    value={50}
-                />
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Icon name="delete" />
+                    <Input
+                        placeholder="Emptied at times..."
+                        onChangeText={text => onChangeEmpty(text)}
+                        value={empty}
+                    />
+                </View>
 
-            </View>
+                <View
+                    style={{
+                        height: "5%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flex: 1,
+                        flexDirection: "row",
+                    }}>
+                    <Text>Cleanliness:</Text>
+                    <Slider
+                        animateTransitions
+                        animationType="timing"
+                        maximumTrackTintColor="#ccc"
+                        maximumValue={100}
+                        minimumTrackTintColor="#222"
+                        minimumValue={0}
+                        onSlidingComplete={value => onChangeClean(value.toString())
+                            }
+                        orientation="horizontal"
+                        step={1}
+                        style={{width: "60%", height: 200, marginLeft: 5}}
+                        thumbStyle={{height: 20, width: 20}}
+                        thumbTouchSize={{width: 40, height: 40}}
+                        trackStyle={{height: 10, borderRadius: 20}}
+                        value={parseInt(cleanliness)}
+                    />
+                </View>
 
-            <View
-                style={{
-                    height: "10%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}>
-                <Button
-                    title="Add photo"
-                    style={{width: " 50%"}}
-                />
-            </View>
+                <View
+                    style={{
+                        height: "10%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}>
+                    <Button title="Add photo" style={{width: " 50%"}} />
+                </View>
 
-            <View
-                style={{
-                    height: "15%",
-                    width: "60%",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}>
-                <Button
-                    title="Save dumpster"
-                    style={{width: " 50%"}}
-                />
+                <View
+                    style={{
+                        height: "15%",
+                        width: "60%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}>
+                    <Button
+                        title="Save dumpster"
+                        style={{width: " 50%"}}
+                        onPress={() => print()}
+                    />
+                </View>
             </View>
         </View>
-    </View>
-);
+    );
+
+    function print() {
+        //console.log(name);
+        //console.log(dumpster);
+        //console.log(store);
+        //console.log(isPositive);
+        //console.log(isLocked);
+        //console.log(empty);
+        console.log(cleanliness);
+    }
 }
 
 const styles = StyleSheet.create({
