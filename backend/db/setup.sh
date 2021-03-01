3e8bfc79c5daff71244c0dd710a9ea3bdd2fc024
Tore Bergebakken <toberge@stud.ntnu.no>
2021-03-01 16:42:13 +0100

    Add wsl support to db makefile

--- backend/db/setup.sh
+++ backend/db/setup.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+source ../api/.env # fetch variables
+
 for script in init data
 do
     echo "Running $script..."
-    mysql --host=127.0.0.1 --user=root --password=thecakeisalive dumpster < "$script".sql
+    mysql --host=127.0.0.1 \
+          --user="${DB_USER:-root}" \
+          --password="${DB_PASSWORD:-thecakeisalive}" \
+          "${DB_NAME:-dumpster}" < "$script".sql
 done
