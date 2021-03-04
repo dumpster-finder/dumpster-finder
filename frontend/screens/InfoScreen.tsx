3a961f88b65e1d8924a64068aec440559c88dde6
Tore Bergebakken <toberge@stud.ntnu.no>
2021-03-04 12:46:23 +0100

    Adjust InfoScreen styling

--- frontend/screens/InfoScreen.tsx
+++ frontend/screens/InfoScreen.tsx
@@ -12,43 +12,45 @@ export default function InfoScreen() {
     return (
         <Layout style={styles.container}>
             <ScrollView style={styles.scrollView}>
-                <View style={{ padding: 10 }}>
-                    <Text category={"h5"}>Icon explanation:</Text>
-                    <View style={{ paddingVertical: 10 }}>
-                        <View style={styles.row}>
-                            <StarIcon size="medium" />
-                            <Text style={styles.textPart}>
-                                Rating for dumpster
-                            </Text>
-                        </View>
-                        <View style={styles.row}>
-                            <LockIcon size="medium" />
-                            <Text style={styles.textPart}>
-                                Is the dumpster locked
-                            </Text>
-                        </View>
-                        <View style={styles.row}>
-                            <TrashIcon size="medium" />
-                            <Text style={styles.textPart}>
-                                Emptying schedule for dumpster
-                            </Text>
-                        </View>
-                        <View style={styles.row}>
-                            <PositiveIcon size="medium" />
-                            <Text style={styles.textPart}>
-                                Stores view on dumpster diving
-                            </Text>
-                        </View>
+                <Text category={"h5"}>Icon explanation:</Text>
+                <View style={{ paddingVertical: 10 }}>
+                    <View style={styles.row}>
+                        <StarIcon size="medium" />
+                        <Text style={styles.textPart}>Rating for dumpster</Text>
                     </View>
+                    <View style={styles.row}>
+                        <LockIcon size="medium" />
+                        <Text style={styles.textPart}>
+                            Is the dumpster locked
+                        </Text>
+                    </View>
+                    <View style={styles.row}>
+                        <TrashIcon size="medium" />
+                        <Text style={styles.textPart}>
+                            Emptying schedule for dumpster
+                        </Text>
+                    </View>
+                    <View style={styles.row}>
+                        <PositiveIcon size="medium" />
+                        <Text style={styles.textPart}>
+                            Stores view on dumpster diving
+                        </Text>
+                    </View>
+                </View>
 
-                    <Text category={"h5"}>Dumpster diving advice: </Text>
-                    <View style={{ paddingVertical: 10 }}>
-                        <Text style={styles.text}>1. Don’t go diving in locked dumpsters</Text>
-                        <Text style={styles.text}>2. Don’t go in dumpsters marked private</Text>
-                        <Text style={styles.text}>3. Don’t go dumpster diving alone</Text>
-                        <Text style={styles.text}>4. Dress accordingly</Text>
-                        <Text style={styles.text}>5. Clean up after yourself</Text>
-                    </View>
+                <Text category={"h5"}>Dumpster diving advice: </Text>
+                <View style={{ paddingVertical: 10 }}>
+                    <Text style={styles.text}>
+                        1. Don’t go diving in locked dumpsters
+                    </Text>
+                    <Text style={styles.text}>
+                        2. Don’t go in dumpsters marked private
+                    </Text>
+                    <Text style={styles.text}>
+                        3. Don’t go dumpster diving alone
+                    </Text>
+                    <Text style={styles.text}>4. Dress accordingly</Text>
+                    <Text style={styles.text}>5. Clean up after yourself</Text>
                 </View>
             </ScrollView>
         </Layout>
@@ -62,13 +64,14 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     scrollView: {
-        width: "100%",
+        width: "auto",
+        marginVertical: "15%",
     },
     row: {
         flex: 1,
         flexDirection: "row",
         paddingVertical: 5,
-        paddingHorizontal: 5
+        paddingHorizontal: 5,
     },
     title: {
         alignSelf: "center",
@@ -77,7 +80,7 @@ const styles = StyleSheet.create({
     },
     text: {
         paddingVertical: 5,
-        paddingHorizontal: 5
+        paddingHorizontal: 5,
     },
     separator: {
         marginVertical: 30,
