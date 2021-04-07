a60bbe60ef34abfc2282c44a5fe6776df7d5e756
Tore Bergebakken <toberge@stud.ntnu.no>
2021-04-07 12:33:46 +0200

    Fix negative ratings not being possible
    Found a serious issue randomly...
    It's fixed now, and I wrote a few new tests to *actually check this*

--- backend/api/daos/__tests__/comments.spec.ts
+++ backend/api/daos/__tests__/comments.spec.ts
@@ -1,7 +1,6 @@
 import { setupTestData } from "../../config/testSetup";
 import Models from "../../models";
 import CommentDAO from "../comments";
-import { Comments } from "../../models/Comments";
 
 const commentDAO = CommentDAO(Models);
 
@@ -34,9 +33,25 @@ describe("updateOne", () => {
     it("should change a comment so the rating is higher", async () => {
         const comment = await commentDAO.updateOne(1, 1);
         const commentAfter = await commentDAO.getAllForDumpster(1);
-        const changedComment = commentAfter.find(com => (com.commentID = 1));
-        expect(comment).not.toBe(undefined);
-        // @ts-ignore
-        expect(changedComment.rating).toBe(6);
+        const changedComment = commentAfter.find(com => com.commentID === 1);
+        expect(changedComment).not.toBe(undefined);
+        // Expect it to have risen to 6 = 5 + 1
+        if (changedComment) expect(changedComment.rating).toBe(6);
+    });
+    it("should change a comment so the rating is lower", async () => {
+        const comment = await commentDAO.updateOne(2, -1);
+        const commentAfter = await commentDAO.getAllForDumpster(1);
+        const changedComment = commentAfter.find(com => com.commentID === 2);
+        expect(changedComment).not.toBe(undefined);
+        // Expect it to lower to 5 = 6 - 1
+        if (changedComment) expect(changedComment.rating).toBe(5);
+    });
+    it("should be able to result in a negative rating", async () => {
+        const comment = await commentDAO.updateOne(4, -1);
+        const commentAfter = await commentDAO.getAllForDumpster(2);
+        const changedComment = commentAfter.find(com => com.commentID === 4);
+        expect(changedComment).not.toBe(undefined);
+        // Expect it to lower to -1 = 0 - 1
+        if (changedComment) expect(changedComment.rating).toBe(-1);
     });
 });
