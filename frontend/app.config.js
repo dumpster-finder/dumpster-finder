80681f50fef51a4beebeda4b0b6ae63db2ead8ef
Tore Bergebakken <toberge@stud.ntnu.no>
2021-04-27 17:37:40 +0200

    Make build_app stage actually upload a build

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -24,6 +24,7 @@ export default {
     assetBundlePatterns: ["**/*"],
     ios: {
         supportsTablet: true,
+        bundleIdentifier: "org.dumpsterfire.dumpsterfinder",
     },
     android: {
         adaptiveIcon: {
