f2510ef1d16ab531053db25fc87bb7e5adb635e3
Tore Bergebakken <toberge@stud.ntnu.no>
2021-05-07 12:20:42 +0200

    Remove unnecessary response data

--- backend/api/routes/comments.ts
+++ backend/api/routes/comments.ts
@@ -279,7 +279,7 @@ export default function({ Models }: RouteDependencies) {
      *         description: JWT for authentication
      *     responses:
      *       "204":
-     *         description: Number of affected rows
+     *         description: Successful removal
      */
     router.delete(
         "/:commentID",
