8124e3e543f09c216e918aa1084d50459e9509a2
Tore Bergebakken <toberge@stud.ntnu.no>
2021-05-20 17:19:39 +0200

    Remove userID from comment model in frontend

--- frontend/models/Comment.ts
+++ frontend/models/Comment.ts
@@ -2,7 +2,6 @@ export interface RawComment {
     commentID: number;
     dumpsterID: number;
     nickname: string;
-    userID: number;
     comment: string;
     rating: number;
     date: string;
@@ -13,7 +12,6 @@ class Comments {
     commentID: number;
     dumpsterID: number;
     nickname: string;
-    userID: number;
     comment: string;
     rating: number;
     date: Date;
@@ -23,7 +21,6 @@ class Comments {
         commentID,
         dumpsterID,
         nickname,
-        userID,
         comment,
         rating,
         date,
@@ -32,7 +29,6 @@ class Comments {
         this.commentID = commentID;
         this.dumpsterID = dumpsterID;
         this.nickname = nickname;
-        this.userID = userID;
         this.comment = comment;
         this.rating = rating;
         this.date = new Date(date);
